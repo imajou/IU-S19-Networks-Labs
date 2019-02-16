fb82cfb4be7c6bd9f97f6cba98a9c77656e7d334
Gleb Petrakov <g.petrakov@innopolis.ru>
2019-02-16 18:49:39 +0300

    Week 4

---
+++ week04/src/common.h
@@ -0,0 +1,14 @@
+typedef struct _test_struct
+{
+
+    char name[64];
+    unsigned int age;
+    unsigned int group;
+} test_struct_t;
+
+typedef struct result_struct_
+{
+
+    char data[1024];
+
+} result_struct_t;
